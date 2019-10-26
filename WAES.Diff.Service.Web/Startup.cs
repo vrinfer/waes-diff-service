8e4aab51f72c1bb7c9b2c8b0e73cc5c14579e8f3
Valeria <valeinfer@gmail.com>
2019-10-25 23:01:46 -0300

    Add swagger

--- WAES.Diff.Service.Web/Startup.cs
+++ WAES.Diff.Service.Web/Startup.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
 
 namespace WAES.Diff.Service.Web
 {
@@ -25,14 +19,31 @@ namespace WAES.Diff.Service.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSwaggerGen(config =>
+            {
+                config.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "WAES Diff Service",
+                    Description = "A simple diff API",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Valeria Infer",
+                        Email = "valeinfer@gmail.com",
+                        Url = new Uri("https://github.com/vrinfer"),
+                    }
+                });
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            app.UseSwagger();
+            app.UseSwaggerUI(config =>
             {
-                app.UseDeveloperExceptionPage();
-            }
+                config.SwaggerEndpoint("/swagger/v1/swagger.json", "WAES Diff Service");
+            });
 
             app.UseHttpsRedirection();
 
